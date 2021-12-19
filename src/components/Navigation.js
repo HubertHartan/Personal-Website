e5ffbb7750fbed059a40bd3f358b3074dad281cd
Hubert Hartan <hubert.hartan@students.mq.edu.au>
2021-12-19 18:08:44 +1100

    Navigation skeleton done

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,13 +1,53 @@
-import React from 'react'
+import React, {useState} from 'react'
 import {Link} from 'react-router-dom'
 
 const Navigation = () =>{
+	const [click, setClick] = useState(false);
+
+	const handleClick = () =>{
+		setClick(!click)
+	}
+	
+	const closeMenu = () =>{
+		setClick(false)
+ 	}
 	return(
 		<nav className="navigation">
 			<div className="navigation-container">
 				<Link to="/" className="navigation-logo">
-					1.Home
+					Logo
 				</Link>
+				<div className="menu-icon" onClick={handleClick}>
+					<i className={click ? "fas fa-times":"fas fa-bars"}/>
+				</div>
+
+				<ul className={click ? "nav-menu active" : "nav-menu inactive"}>
+					<li className="nav-item">
+						<Link to="/" className="nav-link" onClick={closeMenu}>
+							Home
+						</Link>
+					</li>
+
+					<li className="nav-item">
+						<Link to="/projects" className="nav-link" onClick={closeMenu}>
+							Projects
+						</Link>
+					</li>
+
+					<li className="nav-item">
+						<Link to="/contacts" className="nav-link" onClick={closeMenu}>
+							Contacts
+						</Link>
+					</li>
+
+					<li className="nav-item">
+						<Link to="/misc" className="nav-link" onClick={closeMenu}>
+							Misc
+						</Link>
+					</li>
+
+				</ul>
+
 			</div>
 		</nav>
 
